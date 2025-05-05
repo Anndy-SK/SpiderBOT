a2ff3efdb8fc255602bd853a14a2ab23cf4692ac
Andrej Klein <andrejklein123@gmail.com>
2025-05-05 12:24:33 +0200

    new update server and website

---
+++ led-server/public/app.js
@@ -0,0 +1,24 @@
+const { response } = require("express");
+
+// Connect buttons
+const ledOnButton = document.getElementById("led-on");
+const ledOffButton = document.getElementById("led-off");
+const statusText = document.getElementById("status");
+
+// Setting state LED
+function updateStatus() {
+  fetch("/led/status")
+    .then((response) => response.json())
+    .then((data) => {
+      statusText.textContent = `State: ${data.status}`;
+    });
+}
+
+// Event listeners for buttons
+ledOnButton.addEventListener("click", () => {
+  fetch("/led/on", { method: "POST" }).then(() => updateStatus());
+});
+
+ledOffButton.addEventListener("click", () => {
+  fetch("/led/off", { method: "POST" }).then(() => updateStatus());
+});
---
+++ led-server/server.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const { Gpio } = require("onoff");
+
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+const led = new Gpio(17, "out");
+
+// Setting CORS for frontend
+const cors = require("cors");
+app.use(cors());
+app.use(express.json());
+
+// Server static files (HTML, CSS, JS)
+app.use(express.static("public"));
+
+// Endpoints for handling with LEDs
+app.get("/led/on", (req, res) => {
+  led.writeSync(1);
+  res.send({ status: "LED is ON" });
+});
+
+app.get("/led/off", (req, res) => {
+  led.writeSync(0);
+  res.send({ status: "LED is OFF" });
+});
+
+// ochrana pri kill ctrl+c
+process.on("SIGINT", () => {
+  led.unexport();
+  process.exit();
+});
+
+app.listen(PORT, () => {
+  console.log(`The server is starting on https://192.168.137.138:${PORT}`);
+});
